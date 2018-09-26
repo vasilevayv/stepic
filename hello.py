766a6a82bfb9612288337966c14839f748fe68ca
vasilevayv <vasilevayv@gmail.com>
2018-09-26 17:18:51 +0300

    wsgi script v2

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ def app(env, start_resp):
     status = '200 OK'
     headers = [
         ('Content-Type', 'text/plain')
-        ]
-    body = [i for i in env[QUERY_STRING].split('&')]
+    ]
+    body = [i for i in env['QUERY_STRING'].split('&')]
     start_resp(status, headers)
     return body
