7c6f426fcb424208da9b27b010cbfa7e36e2ed3a
vasilevayv <43571203+vasilevayv@users.noreply.github.com>
2018-10-10 12:14:27 +0300

    [*] change url to real views
    main page, popular, question

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -15,15 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from qa.views import test
+from qa.views import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', test),
     url(r'^login/$', test),
     url(r'^signup/$', test),
-    url(r'^question/<([0-9]+)>/$', test),
+    url(r'^question/<([0-9]+)>/$', question),
     url(r'^ask/$', test),
-    url(r'^popular/$', test),
+    url(r'^popular/$', popular),
     url(r'^new/$', test),
+    url(r'^$', main_page),
 ]
